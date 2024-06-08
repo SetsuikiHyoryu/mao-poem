ff5201c3a21a5f83704e632c1502e67af829a144
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2024-06-09 03:46:33 +0900

    fix: update dist

--- dist-tsc/components/body.js
+++ dist-tsc/components/body.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.body = void 0;
-const POEM = '一从大地起风雷，便有精生白骨堆。\n僧是愚氓犹可训，妖为鬼域必成灾。\n金猴奋起千钧棒，玉宇澄清万里埃。\n今日欢呼孙大圣，只缘妖雾又重来。';
+const POEM = '一从大地起风雷，便有精生白骨堆。\n僧是愚氓犹可训，妖为鬼蜮必成灾。\n金猴奋起千钧棒，玉宇澄清万里埃。\n今日欢呼孙大圣，只缘妖雾又重来。';
 const body = document.createElement('p');
 exports.body = body;
 body.textContent = POEM;
